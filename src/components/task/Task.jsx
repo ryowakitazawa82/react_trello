33d25f594b7c4dce1f890d7d8075c0434fb116b6
ryowakitazawa82 <ryowa.kitazawa@gmail.com>
2021-11-05 01:14:34 +0900

    36

--- src/components/task/Task.jsx
+++ src/components/task/Task.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
+import { Draggable } from 'react-beautiful-dnd';
 
 export const Task = ({ task,taskList, setTaskList }) => {
     const handleDelete = (id) => {
         setTaskList(taskList.filter((task) => task.id !== id));
     };
     return (
-        <div　className="taskBox">
+        <Draggable index={task.id} draggableId={task.draggableId}>
+            {(provided) => (
+             <div　className="taskBox" key={task.id} ref={provided.innerRef}
+             {...provided.draggableProps}
+             {...provided.dragHandleProps}
+             >
              <p className="taskText">{ task.text }</p>
              <button className="taskTrashButton">
                  <i className="fas fa-trash-alt" onClick={() => handleDelete(task.id)}></i>
             </button>
         </div>
+            )}
+        </Draggable>
     );
 };
 
