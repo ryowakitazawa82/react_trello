a9e6433e5d2e722c84067435353797f607d822f0
ryowakitazawa82 <ryowa.kitazawa@gmail.com>
2021-11-06 17:41:44 +0900

    finished section6

--- src/components/task/input/TaskAddInput.jsx
+++ src/components/task/input/TaskAddInput.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { v4 as uuid } from 'uuid';
 
 export const TaskAddInput = ({
     inputText,
@@ -7,6 +8,7 @@ export const TaskAddInput = ({
     taskList,
 }) => {
     const handleSubmit = (e) => {
+        const taskId = uuid();
         e.preventDefault();
         if(inputText === "") {
             return;
@@ -17,7 +19,7 @@ export const TaskAddInput = ({
             {
                 id: taskList.length,
                 text: inputText,
-                draggableId: `task-${taskList.length}`,
+                draggableId: `task-${taskId}`,
             },
         ]);
         setInputText("");
